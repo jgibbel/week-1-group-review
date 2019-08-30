b60375e30cc6e700ec5b08c71e53148510ac2e9e
jgibbel <jamesgibbel@gmail.com>
2019-08-30 10:42:34 -0400

    update

--- review-question-3.rb
+++ review-question-3.rb
@@ -10,6 +10,15 @@ class User
   def initialize(name)
     @name = name 
     @photos = []
+    @@all << self
+  end 
+
+  def photos 
+    Photo.all.select {|photo| photo.user == self}
+  end 
+
+  def self.all 
+    @@all 
   end 
   
 end 
@@ -25,31 +34,41 @@ class Photo
   end 
   
   def comments
-    Comments.all.select {|comment| comment.photo == self}
+    Comment.all.select {|comment| comment.photo == self}
   end 
   
   def make_comment(text)
     Comment.new(self, text)
+  end 
+
+  def self.all 
+    @@all 
   end 
   
 end 
 
 class Comment 
   @@all = []
+  attr_reader :photo, :text
   def initialize(photo, text)
     @photo = photo 
     @text = text
     @@all << self 
   end 
+
+  def self.all 
+    @@all 
+  end 
+
 end 
 
 
 sandwich_photo = Photo.new
 sophie = User.new("Sophie")
 sandwich_photo.user = sophie
-sandwich_photo.user.name
+puts sandwich_photo.user.name
 # => "Sophie"
-sophie.photos
+puts sophie.photos
 # => [#<Photo:0x00007fae2880b370>]
 
 
@@ -57,8 +76,8 @@ sandwich_photo.comments
 # => []
 
 sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
-sandwich_photo.comments
+puts sandwich_photo.comments
 # => [#<Comment:0x00007fae28043700>]
 
-Comment.all
+puts Comment.all
 #=> [#<Comment:0x00007fae28043700>]
